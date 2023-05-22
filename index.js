1c96cae8df8683ad6bc76d14a00021d6e83e144b
Raj Poudel <raj.poudeel@gmail.com>
2023-05-22 20:24:31 +0545

    JS get updated with minor changes

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ window.addEventListener('scroll' , () =>{
     text.style.marginTop = value * 2.5 +'px';
     treeLeft.style.left = value * -1.5 +'px';
     treeRight.style.left = value * 0.5 +'px';
-    gateLeft.style.left = value * 0.5 +'px'
-    gateRight.style.right = value * -0.5 +'px'
+    gateLeft.style.left = value * 0.5 +'px';
+    gateRight.style.right = value * -0.5 +'px';
+
+    
 })
